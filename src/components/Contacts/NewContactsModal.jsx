8b330e3eb24aee12c539089a4a4cfc2985b3b2ef
Adaji Mukhtar <alfadaji@gmail.com>
2022-09-04 09:54:00 +0100

    initial project setup

---
+++ src/components/Contacts/NewContactsModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewContactsModal = () => {
+  return (
+    <div>NewContactsModal</div>
+  )
+}
+
+export default NewContactsModal
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,31 @@
+import React, {useRef} from 'react'
+import {v4 as uuidV4} from 'uuid'
+import { Button, Container, Form } from 'react-bootstrap'
+
+const Login = ({onIdSubmit}) => {
+    const idRef = useRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        onIdSubmit(idRef.current.value)
+    }
+    
+    function createNewId() {
+        onIdSubmit(uuidV4())
+    }
+    
+  return (
+    <Container className='d-flex align-items-center justify-content-center' style={{ height: '100vh'}}>
+        <Form className='cw' onSubmit={handleSubmit}>
+            <Form.Group className="mx-auto mb-3">
+                <Form.Label className='fw-bold'>Enter Your ID</Form.Label>
+                <Form.Control type='text' ref={idRef} className="" ></Form.Control>
+            </Form.Group>
+            <Button type='submit' className='me-3'>Login</Button>
+            <Button variant='secondary' onClick={createNewId}>Create an ID</Button>
+        </Form>
+    </Container>
+  )
+}
+
+export default Login
